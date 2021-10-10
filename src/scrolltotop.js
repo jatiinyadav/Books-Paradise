d96463715098493b41023ce2c87a6a597b682269
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-10-10 14:23:16 -0400

    change scroll to top icon

--- src/scrolltotop.js
+++ src/scrolltotop.js
@@ -39,36 +39,14 @@ export default class ScrollToTop extends Component {
     return (
       <div className="scroll-to-top">
         {is_visible && (
-          <div onClick={() => this.scrollToTop()}>
+          <div className="scroll-icon" onClick={() => this.scrollToTop()}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              height="38.735"
-              width="33.749"
+              width="18"
+              height="18"
+              viewBox="0 0 24 24"
             >
-              <g transform="translate(-18.121 -3.364)">
-                <rect
-                  ry="4.928"
-                  y="2.364"
-                  x="18.121"
-                  height="38.735"
-                  width="33.749"
-                  fill="#db4437"
-                />
-                <g transform="translate(-.48 2.134)">
-                  <rect
-                    ry="4.928"
-                    y="1.229"
-                    x="18.601"
-                    height="38.735"
-                    width="33.749"
-                    fill="url(#b)"
-                  />
-                  <g fill="#ececec">
-                    <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
-                    <path d="M22.435 17.62l4.684 4.685 5.044-5.044v19.352h6.625V17.26l5.044 5.044 4.683-4.684-13.04-13.04z" />
-                  </g>
-                </g>
-              </g>
+              <path d="M12 3l12 18h-24z" />
             </svg>
           </div>
         )}
