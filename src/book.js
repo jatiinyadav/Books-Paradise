ae24bbe1f29ced471af25c3c9e5f5a48fb0a1b23
imakish <makishtp@gmail.com>
2021-10-14 13:47:44 +0530

    installed framer motion and added animation to book images

--- src/book.js
+++ src/book.js
@@ -2,101 +2,94 @@ import React from "react";
 import { BiLinkExternal } from "react-icons/bi";
 import defaultBook from "./img/defaultBook.png";
 import "../src/sass/style.css";
+import { motion } from "framer-motion";
 
-const Book = ({id,volumeInfo}) => {
-    let 
-        {
-          title,
-          authors,
-          publisher,
-          previewLink,
-          imageLinks,
-        }
-    = volumeInfo;
+const Book = ({ id, volumeInfo }) => {
+  let { title, authors, publisher, previewLink, imageLinks } = volumeInfo;
 
-    //setting up default values for volume info data
-      title = title || "Title is not available";
-      authors = authors || "Author(s) name not available";
-      publisher = publisher || "Publisher company not available";
-      previewLink = previewLink || "https://books.google.co.in/";
-      
-      return (
-        <section key={id} className="loading-card">
-          <div>
+  //setting up default values for volume info data
+  title = title || "Title is not available";
+  authors = authors || "Author(s) name not available";
+  publisher = publisher || "Publisher company not available";
+  previewLink = previewLink || "https://books.google.co.in/";
+
+  return (
+    <section key={id} className="loading-card">
+      <div>
+        <div>
+          <motion.img
+            src={imageLinks ? imageLinks.thumbnail : defaultBook}
+            width="100px"
+            alt="Book-cover"
+            whileHover={{
+              scale: 1.7,
+              boxShadow: "0px 0px 8px #000",
+            }}
+          />
+        </div>
+        <div>
+          {title && (
             <div>
-              <img
-                src={imageLinks ? imageLinks.thumbnail : defaultBook}
-                width="100px"
-                alt="Book-cover"
-              />
+              <h3 className="inline">{title}</h3>
             </div>
-            <div>
-              {title && (
-                <div>
-                  <h3 className="inline">{title}</h3>
-                </div>
-              )}
-            </div>
+          )}
+        </div>
 
-            <div>
-              {authors && (
-                <h4 style={{ paddingBottom: "1rem", color: "black" }}>
-                  {" "}
-                  Author:{" "}
-                  <span
-                    style={{
-                      fontWeight: "bold",
-                      color: "#3B3B3B",
-                    }}
-                  >
-                    {" "}
-                    {authors}{" "}
-                  </span>
-                </h4>
-              )}
-            </div>
+        <div>
+          {authors && (
+            <h4 style={{ paddingBottom: "1rem", color: "black" }}>
+              {" "}
+              Author:{" "}
+              <span
+                style={{
+                  fontWeight: "bold",
+                  color: "#3B3B3B",
+                }}
+              >
+                {" "}
+                {authors}{" "}
+              </span>
+            </h4>
+          )}
+        </div>
 
-            <div>
-              {publisher && (
-                <h5 style={{ paddingBottom: "1rem", color: "black" }}>
-                  {" "}
-                  Published by:{" "}
-                  <span
-                    style={{
-                      fontWeight: "bold",
-                      color: "#3B3B3B",
-                    }}
-                  >
-                    {" "}
-                    {publisher}{" "}
-                  </span>
-                </h5>
-              )}
-            </div>
+        <div>
+          {publisher && (
+            <h5 style={{ paddingBottom: "1rem", color: "black" }}>
+              {" "}
+              Published by:{" "}
+              <span
+                style={{
+                  fontWeight: "bold",
+                  color: "#3B3B3B",
+                }}
+              >
+                {" "}
+                {publisher}{" "}
+              </span>
+            </h5>
+          )}
+        </div>
 
-            <div>
-              {previewLink && (
-                <h5
-                  style={{
-                    fontWeight: "bold",
-                    color: "black",
-                    paddingBottom: "1rem",
-                  }}
-                >
-                  Read more :{" "}
-                  <a
-                    href={previewLink}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    {" "}
-                    Google Books <BiLinkExternal></BiLinkExternal>{" "}
-                  </a>
-                </h5>
-              )}
-            </div>
+        <div>
+          {previewLink && (
+            <h5
+              style={{
+                fontWeight: "bold",
+                color: "black",
+                paddingBottom: "1rem",
+              }}
+            >
+              Read more :{" "}
+              <a href={previewLink} target="_blank" rel="noreferrer">
+                {" "}
+                Google Books <BiLinkExternal></BiLinkExternal>{" "}
+              </a>
+            </h5>
+          )}
+        </div>
 
-            {/* <div
+        {/* <div
               style={{
                 display: "flex",
                 justifyContent: "center",
@@ -127,9 +120,9 @@ const Book = ({id,volumeInfo}) => {
                 {averageRating}
               </p>
             </div> */}
-          </div>
-        </section>
-      );
-}
+      </div>
+    </section>
+  );
+};
 
-export default Book
+export default Book;
