70eef7b9b1a64ce029b3dfa89804f9ba29b2a1de
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-10-14 20:40:58 -0400

    using arrow for scroll to top button

--- src/scrolltotop.js
+++ src/scrolltotop.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Arrow from "./img/arrow.png";
 
 export default class ScrollToTop extends Component {
   constructor(props) {
@@ -42,11 +43,11 @@ export default class ScrollToTop extends Component {
           <div className="scroll-icon" onClick={() => this.scrollToTop()}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="18"
-              height="18"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
             >
-              <path d="M12 3l12 18h-24z" />
+              <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" />
             </svg>
           </div>
         )}
