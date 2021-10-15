eb61c8e78c8bbe424aeb5ded926c5fb27875d836
tuanthanh2067 <tuanthanh2067@gmail.com>
2021-10-15 12:05:48 -0400

    change dimensions of scroll-to-top button

--- src/scrolltotop.js
+++ src/scrolltotop.js
@@ -43,8 +43,8 @@ export default class ScrollToTop extends Component {
           <div className="scroll-icon" onClick={() => this.scrollToTop()}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
+              width="16"
+              height="16"
               viewBox="0 0 24 24"
             >
               <path d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z" />
